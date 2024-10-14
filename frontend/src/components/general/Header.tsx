3b2d2ff7676da3592ad06e305f3087bbe9cff097
Johan Nyman <johan.nyman97@gmail.com>
2024-10-14 23:29:04 +0200

    update buttons

--- frontend/src/components/general/Header.tsx
+++ frontend/src/components/general/Header.tsx
@@ -2,11 +2,11 @@ import Button from "./Button";
 
 function Header() {
 	return (
-		<div className=" p-16 flex justify-between">
+		<div className="px-16 py-6 flex justify-between items-center border-b">
 			<p>Placeholder logo</p>
 			<div className="flex gap-4">
-				<Button content="sign up" variant="secondary" />
-				<Button content="login" variant="primary" />
+				<Button content="sign up" variant="secondary" urlPath="/signup" />
+				<Button content="login" variant="primary" urlPath="/login" />
 			</div>
 		</div>
 	);
