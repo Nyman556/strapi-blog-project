77278f9bdceb840304174b6e5ccb0e835fdd3c9c
Johan Nyman <johan.nyman97@gmail.com>
2024-10-14 23:12:51 +0200

    react router setup, added layout and error-page.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
+// src/main.tsx (or main.jsx)
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import ErrorPage from "./views/error-page";
+import Layout from "./Layout";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/",
+				element: <App />,
+			},
+		],
+	},
+]);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>
 );
---
+++ frontend/src/views/error-page.tsx
@@ -0,0 +1,18 @@
+import { Link, useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+	const error = useRouteError();
+	const errorMessage =
+		(error as { statusText?: string; message?: string }) || {};
+
+	return (
+		<div className="w-screen h-screen flex flex-col justify-center text-center align-middle">
+			<h1 className=" text-3xl">Oops!</h1>
+			<p>Sorry, an unexpected error has occurred.</p>
+			<p>
+				<i>{errorMessage.statusText || errorMessage.message}</i>
+			</p>
+			<Link to="/">Take me back!</Link>
+		</div>
+	);
+}
