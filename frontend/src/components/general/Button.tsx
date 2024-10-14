fed3aec0c12791b7bf7cf1581b997c87070f6111
Johan Nyman <johan.nyman97@gmail.com>
2024-10-14 23:28:53 +0200

    urlpath conditional

--- frontend/src/components/general/Button.tsx
+++ frontend/src/components/general/Button.tsx
@@ -1,19 +1,26 @@
+import { Link } from "react-router-dom";
+
 interface ButtonProps {
 	content: string;
 	variant: "primary" | "secondary";
+	urlPath?: string;
 }
 
-function Button({ content, variant }: ButtonProps) {
+function Button({ content, variant, urlPath }: ButtonProps) {
 	const styles = {
 		general: "py-2 px-4 rounded capitalize border",
 		primary: "bg-gray-900 text-white",
 		secondary: "bg-white text-gray-900",
 	};
 
-	return (
-		<button className={`${styles.general} ${styles[variant]}`}>
+	const buttonClasses = `${styles.general} ${styles[variant]}`;
+
+	return urlPath ? (
+		<Link to={urlPath} className={buttonClasses}>
 			{content}
-		</button>
+		</Link>
+	) : (
+		<button className={buttonClasses}>{content}</button>
 	);
 }
 
