7cc389f6ee11a54945379c1ef9e76fc618e39a7b
Johan Nyman <johan.nyman97@gmail.com>
2024-10-14 23:50:01 +0200

    logo

--- frontend/src/components/general/Header.tsx
+++ frontend/src/components/general/Header.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import Button from "./Button";
 
 function Header() {
 	return (
 		<div className="px-16 py-6 flex justify-between items-center border-b">
-			<p>Placeholder logo</p>
+			<Link to="/" className=" text-2xl font-semibold">
+				Logo
+			</Link>
 			<div className="flex gap-4">
 				<Button content="sign up" variant="secondary" urlPath="/signup" />
 				<Button content="login" variant="primary" urlPath="/login" />
