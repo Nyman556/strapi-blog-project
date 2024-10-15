cff1219b085cbb43a64a8dafa6abbf023557c854
Johan Nyman <johan.nyman97@gmail.com>
2024-10-15 10:51:04 +0200

    conditional based on user from context, clear user on logout

--- frontend/src/components/general/Header.tsx
+++ frontend/src/components/general/Header.tsx
@@ -1,15 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { useUser } from "../../contexts/UserContext";
 
 function Header() {
+	const { user, clearUser } = useUser();
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		clearUser();
+		navigate("/");
+	};
+
 	return (
 		<div className="px-16 py-6 flex justify-between items-center border-b">
-			<Link to="/" className=" text-2xl font-semibold">
+			<Link to="/" className="text-2xl font-semibold">
 				Logo
 			</Link>
 			<div className="flex gap-4">
-				<Button content="sign up" variant="secondary" urlPath="/signup" />
-				<Button content="login" variant="primary" urlPath="/login" />
+				{user ? (
+					<>
+						<Button
+							content="Create Post"
+							variant="secondary"
+							urlPath="/create-post"
+						/>
+						<Button content="Logout" variant="primary" onClick={handleLogout} />
+					</>
+				) : (
+					<>
+						<Button content="Sign Up" variant="secondary" urlPath="/signup" />
+						<Button content="Login" variant="primary" urlPath="/login" />
+					</>
+				)}
 			</div>
 		</div>
 	);
