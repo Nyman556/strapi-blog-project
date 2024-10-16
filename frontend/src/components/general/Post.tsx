103b1f140a7b8b5974d5ee3b6b2821eec3130c36
Johan Nyman <johan.nyman97@gmail.com>
2024-10-16 18:41:09 +0200

    link to slug instead of id

--- frontend/src/components/general/Post.tsx
+++ frontend/src/components/general/Post.tsx
@@ -15,7 +15,7 @@ function Post({ post }: PostProps) {
 				<p>
 					By {post.author} | {post.date}
 				</p>
-				<Link to={`/${post.id}`} className="flex gap-2 items-center">
+				<Link to={`/${post.slug}`} className="flex gap-2 items-center">
 					Read More <FiChevronRight />
 				</Link>
 			</div>
